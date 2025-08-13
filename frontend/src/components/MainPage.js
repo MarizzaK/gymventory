588c1d4c8ab3789878dae442bcad39b93aa2480f
MK <marizza.karlin@icloud.com>
2025-08-13 16:40:40 +0200

    added components and integrated React MUI

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Inventorytable from "./InventoryTable";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Inventorytable></Inventorytable>
+    </div>
+  );
+};
+
+export default MainPage;
