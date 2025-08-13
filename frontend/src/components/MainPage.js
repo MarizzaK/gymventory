05389377384590bd8d48bf67b963d64d1155d986
MK <marizza.karlin@icloud.com>
2025-08-13 19:40:10 +0200

    Redesigned MUI components

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Inventorytable from "./InventoryTable";
 
 const MainPage = () => {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      handleFetchData();
+    }
+    fetchData();
+  }, []);
+
+  const handleFetchData = async () => {
+    const response = await fetch("http://localhost:5001/api/products");
+    const data = await response.json();
+    console.log(data);
+    const dataWithUniqueIds = data.map((item, index) => ({
+      ...item,
+      id: item._id,
+    }));
+    setRows(dataWithUniqueIds);
+  };
+
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
       <Header></Header>
-      <Inventorytable></Inventorytable>
+      <Inventorytable rows={rows}></Inventorytable>
     </div>
   );
 };
